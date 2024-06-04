b69cba11321868f01939b9fc980c9505e60468af
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-06-04 14:47:08 +0900

    test: add test case

---
+++ src/utils_test.ts
@@ -0,0 +1,31 @@
+import { resolveURL } from "./utils.ts";
+import { describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+import { resolve, toFileUrl } from "@std/path";
+
+describe("resolveURL", () => {
+  it("should return same", () => {
+    const table: URL[] = [
+      new URL("file:///"),
+      new URL("https://test.test"),
+    ];
+
+    table.forEach((url) => {
+      expect(resolveURL(url)).toBe(url);
+    });
+  });
+
+  it("should return resolved url", () => {
+    const table: [string, URL | string][] = [
+      ["./main.ts", toFileUrl(resolve("./main.ts"))],
+      ["a", toFileUrl(resolve("a"))],
+      ["../main.ts", toFileUrl(resolve("../main.ts"))],
+      ["/", "file:///"],
+      ["/a/b", "file:///a/b"],
+    ];
+
+    table.forEach(([url, expected]) => {
+      expect(resolveURL(url)).toEqual(new URL(expected));
+    });
+  });
+});
