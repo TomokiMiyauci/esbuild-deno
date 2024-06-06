8a8842689bf5e010989b12ef0d72cb5fef7c74b3
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-06-06 12:27:31 +0900

    test(tsconfig_raw): add test case

--- src/tsconfig_raw_test.ts
+++ src/tsconfig_raw_test.ts
@@ -4,11 +4,14 @@ import {
   assertCompilerOptionsType,
   assertPaths,
   assertTsconfigRaw,
+  mergeTsconfigRaw,
+  normalizeTsconfigRaw,
   parseTsconfigRaw,
 } from "./tsconfig_raw.ts";
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
 import { JsonValue } from "@std/jsonc";
+import { TsconfigRaw } from "esbuild";
 
 describe("parseTsconfigRaw", () => {
   it("should throw error if the input is invalid JSON format", () => {
@@ -158,5 +161,56 @@ describe("assertPaths", () => {
     expect(assertPaths({ "": ["", "a", "b", "c"] })).toBeFalsy();
     expect(assertPaths({ "a": ["", "a", "b", "c"], "b": ["d", "e", "f"] }))
       .toBeFalsy();
+  });
+});
+
+describe("normalizeTsconfigRaw", () => {
+  it("should return empty object if input is undefined", () => {
+    expect(normalizeTsconfigRaw(undefined)).toEqual({});
+  });
+
+  it("should return parsed object if input is string", () => {
+    expect(normalizeTsconfigRaw(`{ "compilerOptions": { "jsx": "react" } }`))
+      .toEqual({ compilerOptions: { jsx: "react" } });
+  });
+
+  it("should return same if input is object", () => {
+    const input = {};
+    expect(normalizeTsconfigRaw(input)).toBe(input);
+  });
+});
+
+describe("mergeTsconfigRaw", () => {
+  it("should be merged shallow with priority given to the right", () => {
+    const table: [TsconfigRaw, TsconfigRaw, TsconfigRaw][] = [
+      [{}, {}, { compilerOptions: {} }],
+      [{ compilerOptions: {} }, {}, { compilerOptions: {} }],
+      [{}, { compilerOptions: {} }, { compilerOptions: {} }],
+      [{ compilerOptions: {} }, { compilerOptions: {} }, {
+        compilerOptions: {},
+      }],
+      [{ compilerOptions: { jsx: "preserve" } }, { compilerOptions: {} }, {
+        compilerOptions: { jsx: "preserve" },
+      }],
+      [{ compilerOptions: { jsx: "preserve" } }, {
+        compilerOptions: { jsx: "react-jsx" },
+      }, {
+        compilerOptions: { jsx: "react-jsx" },
+      }],
+      [{ compilerOptions: { jsx: "preserve" } }, {
+        compilerOptions: { importsNotUsedAsValues: "error" },
+      }, {
+        compilerOptions: { jsx: "preserve", importsNotUsedAsValues: "error" },
+      }],
+      [{ compilerOptions: { paths: { a: [] } } }, {
+        compilerOptions: { paths: { b: [] } },
+      }, {
+        compilerOptions: { paths: { b: [] } },
+      }],
+    ];
+
+    table.forEach(([left, right, expected]) => {
+      expect(mergeTsconfigRaw(left, right)).toEqual(expected);
+    });
   });
 });
