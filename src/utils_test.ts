9dd5b35097fbba4440e48f446e47736251d78a95
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-06-09 17:12:56 +0900

    test: add test case

--- src/utils_test.ts
+++ src/utils_test.ts
@@ -1,4 +1,10 @@
-import { isObject, resolvePath } from "./utils.ts";
+import {
+  formatImportMapDiagnostics,
+  isObject,
+  listed,
+  resolvePath,
+  tabbed,
+} from "./utils.ts";
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
 import { join } from "@std/path";
@@ -48,5 +54,40 @@ describe("isObject", () => {
     table.forEach((input) => {
       expect(isObject(input)).toBeFalsy();
     });
+  });
+});
+
+describe("formatImportMapDiagnostics", () => {
+  it("should", () => {
+    const table: [string[], string][] = [
+      [[], `Import map diagnostics:\n`],
+      [
+        ["reason1"],
+        `Import map diagnostics:
+  - reason1`,
+      ],
+      [
+        ["reason1", "reason2", "reason3"],
+        `Import map diagnostics:
+  - reason1
+  - reason2
+  - reason3`,
+      ],
+    ];
+
+    for (const [input, expected] of table) {
+      expect(formatImportMapDiagnostics(input)).toBe(expected);
+    }
+  });
+});
+
+describe("listed", () => {
+  it("should pass", () => expect(listed("abc")).toBe("- abc"));
+});
+
+describe("tabbed", () => {
+  it("should pass", () => {
+    expect(tabbed(2)("abc")).toBe("  abc");
+    expect(tabbed(4)("abc")).toBe("    abc");
   });
 });
