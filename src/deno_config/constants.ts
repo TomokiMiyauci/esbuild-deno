937c4f581bcce1a7c4182a3f7f18960fa1866972
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-06-11 10:50:00 +0900

    chore(constants): change error message

--- src/deno_config/constants.ts
+++ src/deno_config/constants.ts
@@ -1,5 +1,5 @@
 export const enum Message {
   InvalidJsonFormat = "Unable to parse config file JSON '{url}'",
-  InvalidJsonType = "config file JSON '{url}' should be an object",
-  FailReadConfig = "reading config file '{url}'",
+  InvalidJsonType = "Error config file JSON '{url}' should be an object",
+  FailReadConfig = "Error reading config file '{url}'",
 }
